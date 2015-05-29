fc94ec1c8a3346b0d88fcd2692790e37a7cb9f99
vishnu <vishnucherumadathil@gmail.com>
2015-05-29 13:47:13 -0400

    changed int to biginteger field

---
+++ apps/posts/templatetags/category_list.py
@@ -0,0 +1,9 @@
+from django import template
+from apps.posts.models import *
+
+register = template.Library()
+
+@register.assignment_tag()
+def list_category():
+    categories = Category.objects.all()
+    return categories
