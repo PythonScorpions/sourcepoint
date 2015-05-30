74e341e5fa61fa2b20eb0423499d2c9f2797540a
vishnu <vishnucherumadathil@gmail.com>
2015-05-30 14:31:06 -0400

    modules for listing,edit,deleting posts of logged in users

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns("apps.posts.views",
                        url(r'^category/(?P<category>[\w-]+)/type/(?P<type>\w+)/$', CategoryList.as_view(), name='category-post'),
                        url(r'^post-detail/(?P<id>\d+)/$', PostDetail.as_view(), name='post-detail'),
                        url(r'^my-posting/$',MyPosting.as_view(), name='my-posting'),
-                       url(r'^posting-details/(?P<id>\d+)/$', MyPostDetail.as_view(), name='post-detail'),
+                       url(r'^posting-details/(?P<id>\d+)/$', MyPostDetail.as_view(), name='posting-detail'),
                        url(r'^post-delete/(?P<id>\d+)/$','postdelete', name='delete-post')
                        )
