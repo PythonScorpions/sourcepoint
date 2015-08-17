aaedd82eda6826f6cbcfb8a53017cb83530eb322
vishnu <scorpionspython@gmail.com>
2015-08-18 00:40:00 +0530

    added amount in lsit display

--- apps/payment/admin.py
+++ apps/payment/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from apps.payment.models import Payment
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['user', 'payment_type', 'date']
+    list_display = ['user', 'amount', 'payment_type', 'date']
 
 admin.site.register(Payment, PaymentAdmin)
